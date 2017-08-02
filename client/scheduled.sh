0a37192e29863abf54633947db9a5434a3993841
Roman Karachinsky <romirez@gmail.com>
2017-08-02 06:23:35 +0000

    Initial client commit

---
+++ client/scheduled.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+(sleep 5 && python /home/pi/update.py) &
+(sleep 10 && python /home/pi/update.py) &
+(sleep 15 && python /home/pi/update.py) &
+(sleep 20 && python /home/pi/update.py) &
+(sleep 25 && python /home/pi/update.py) &
+(sleep 30 && python /home/pi/update.py) &
+(sleep 35 && python /home/pi/update.py) &
+(sleep 40 && python /home/pi/update.py) &
+(sleep 45 && python /home/pi/update.py) &
+(sleep 50 && python /home/pi/update.py) &
+(sleep 55 && python /home/pi/update.py) &
